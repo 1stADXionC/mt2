ee8870ba9265f80407f7f6fdc098707d9c4a010f
1stADXionC <rziemianek2503@gmail.com>
2026-04-28 17:27:39 +0200

    Add files via upload

---
+++ server/common/service.h
@@ -0,0 +1,29 @@
+#ifndef __INC_SERVICE_H__
+#define __INC_SERVICE_H__
+#pragma once
+
+//  --------------------------------------------
+// |	Stock Defines							|
+//  --------------------------------------------
+#define _IMPROVED_PACKET_ENCRYPTION_			
+#define __PET_SYSTEM__
+#define __UDP_BLOCK__
+
+#define ENABLE_GUILDRENEWAL_SYSTEM															
+#if defined(ENABLE_GUILDRENEWAL_SYSTEM)
+	#define ENABLE_SHOW_LIDER_AND_GENERAL_GUILD 																								//Guerras de gremio (Rondas, puntos, tiempo)
+#endif
+#define ENABLE_MEMLEKET_SYSTEM	
+
+//  --------------------------------------------
+// |	New Defines								|
+//  --------------------------------------------
+#define CLIENT_LOCALE_STRING					
+
+//  --------------------------------------------
+// |	Fixes									|
+//  --------------------------------------------
+#define LEVEL_FIX								
+
+//  ============================================
+#endif
